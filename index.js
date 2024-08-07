5cf159543d99e814011497c5f96302223ebc2da7
Ankush Rawat <ankushrawat037@gmail.com>
2024-08-07 13:20:50 +0530

    vercel updates

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const app = require("./app.js")
 
 const port = process.env.PORT || 3000
 
-app.listen(port , ()=>{
-    console.log("server is running on port " , port)
-})
+// app.listen(port , ()=>{
+//     console.log("server is running on port " , port)
+// })
+
+module.exports = app
